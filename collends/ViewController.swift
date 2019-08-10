16cad8979c0d2ba87ec1b336d305268e73f65601
Yamagn <syankenpon@gmail.com>
2019-08-10 17:57:30 +0900

    Initial Commit

---
+++ collends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collends
+//
+//  Created by ymgn on 2019/08/10.
+//  Copyright © 2019 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
